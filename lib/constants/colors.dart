9ee08228a50687c3b807d0e077a0158cb8f4a946
Examinationmaestro <examinationmaestro@gmail.com>
2023-07-25 15:28:55 +0530

    initial commit

---
+++ lib/constants/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const bgColor = Color(0xff000000);
+  static const primaryColor = Color(0xfffff2c5);
+  static const secondaryColor = Color(0xffffe8e5);
+  static const cardColor = Color(0xffe0f1ff);
+  static const blackColor = Color(0xff000000);
+  static const whiteColor = Color(0xffffffff);
+  static const highlightColor = Color.fromARGB(170, 224, 241, 255);
+  static const transparentColor = Colors.transparent;
+}
